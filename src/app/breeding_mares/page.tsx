2d89069846ca3e5d94aab50c47a473c14113281a
arthur <arthur.o.f.miranda2011@gmail.com>
2025-06-06 11:38:10 -0300

    feat: create breeding_mares body

---
+++ src/app/breeding_mares/page.tsx
@@ -0,0 +1,10 @@
+import SideBar from "../components/sidebar";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen w-full flex bg-amber-50">
+      <SideBar />
+      
+    </div>
+  );
+}
