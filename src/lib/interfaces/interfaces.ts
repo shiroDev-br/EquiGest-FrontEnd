b922a856effe58d4d338b8b634e0a1acca5d2fc6
arthur <arthur.o.f.miranda2011@gmail.com>
2025-06-13 09:02:00 -0300

    feat: create interfaces file

--- src/lib/interfaces/interfaces.ts
+++ src/lib/interfaces/interfaces.ts
@@ -4,18 +4,24 @@ export interface IRegisterRequestBody {
     email: string;
     cellphone: string;
     cpf_cnpj: string;
-  }
+}
   
-export interface IAuthSuccessResponse {
-    access_token: string;
-    token_type: string;
-  }
-  
-export interface IRequestErrorResponse {
-    detail: string;
-  }
-
 export interface ILoginRequestBody {
     username: string;
     password: string;
+}
+
+export interface IRegisterBreedingMareBody {
+   mare_name: string;
+   stallion_name: string;
+   pregnancy_date: Date;
+}
+
+export interface IAuthSuccessResponse {
+    access_token: string;
+    token_type: string;
+}
+  
+export interface IRequestErrorResponse {
+    detail: string;
 }
